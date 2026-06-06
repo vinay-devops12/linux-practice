e6cbce4b25a12a9633982035caa549b12231f4d9
vinay-devops-12 <vinaypayyavula9@gmail.com>
2026-06-06 10:54:26 +0530

    git origin

--- 06-special.variable.sh
+++ 06-special.variable.sh
@@ -3,4 +3,4 @@
 echo "all variables passed to script:$@"
 echo "number of variables passed to script:$#"
 echo "script name :$0"
-echo "who is running this :$user"
+echo "who is running this :$USER"
