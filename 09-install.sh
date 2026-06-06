a62ab9bea3a5d37e1cda132c336dfb225a828cd0
vinay-devops-12 <vinaypayyavula9@gmail.com>
2026-06-06 17:31:17 +0530

    adding linux-practice files

--- 09-install.sh
+++ 09-install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-USERID =$(ID -U)
-if [ $USERID -NE 0]; then
+
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
    echo "please run this script with root access"
    exit 1
 fi
-   echo "iam continue...."
-   
+echo "iam continue...."
