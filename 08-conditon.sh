4e5f4fa146e6a5baa93c0a3b1a4f277721aeab89
vinay-devops-12 <vinaypayyavula9@gmail.com>
2026-06-06 13:08:28 +0530

    git practice

--- 08-conditon.sh
+++ 08-conditon.sh
@@ -7,3 +7,14 @@ if [ $num -ge 20 ];then
 else 
 echo "given number is less then 20"
 fi
+
+
+# next methof
+
+#conditions
+num=$1
+if [ $num -ge 20 ];then
+   echo "given number $num is greather then or equal 20"
+else 
+echo "given number is less then 20"
+fi
