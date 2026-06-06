8b7aec44cef0859d725f1b3408cc43d7885f1942
vinay-devops-12 <vinaypayyavula9@gmail.com>
2026-06-06 11:33:58 +0530

    git origin

--- 06-special.variable.sh
+++ 06-special.variable.sh
@@ -8,4 +8,6 @@ echo "which directory :$pwd"
 echo "PID of the current script:$$"
 sleep 5 &
 echo "PID of the background command running just now :$!"
+## script line no 
+echo" line number :$LINENO"
 
