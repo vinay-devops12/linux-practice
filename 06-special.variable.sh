4be4f147834051499ac232a58713293e91951ec4
vinay-devops-12 <vinaypayyavula9@gmail.com>
2026-06-06 11:24:04 +0530

    git origin

--- 06-special.variable.sh
+++ 06-special.variable.sh
@@ -4,7 +4,7 @@ echo "all variables passed to script:$@"
 echo "number of variables passed to script:$#"
 echo "script name :$0"
 echo "who is running this :$USER"
-echo "which directory :pwd"
+echo "which directory :$pwd"
 echo "PID of the current script:$$"
 echo "PID of the background command running just now :$!"
 
