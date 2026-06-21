c1f34da1c8775b9c34421160f32d96a10363c354
vinay-devops-12 <vinaypayyavula9@gmail.com>
2026-06-21 10:09:40 +0530

    Added mongo install script and repo file

---
+++ shell-roboshop/01-mongodb.sh
@@ -0,0 +1,35 @@
+
+
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$(basename $0).log"
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date "+%Y-%m-%d-%H:%M:%S")
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$TIMESTAMP [ERROR] $R YOU SHOULD BE RUN AS ROOT USER $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $R $2...FAILED $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$TIMESTAMP [INFO] $G $2...SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp mango.repo /etc/yum.repos.d/mango.repo &>> $LOGS_FILE
+VALIDATE $? "Adding mongo repo"
